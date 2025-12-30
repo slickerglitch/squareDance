fca123bef9914d4bf51c12d79fbda34d2797ca3d
JK <slickerglitch@gmail.com>
2025-12-30 01:04:31 -0600

    Small changes to board structure and associated nomenclature

--- script.js
+++ script.js
@@ -1,41 +1,30 @@
 // Creating a game object to track curr player & board state
-const ticTacToe = {
+const game = {
 	currPlayer: "x", // <- Track current player
-	boardState: Array(9).fill(null), // Initialize to null so ready to add player inputs (either X || O)
+	boardState: Array(12).fill(null), // Initialize to null so ready to add player inputs (either X || O)
 	gameOver: false,
 	// Initialize the gameboard
 
 	gameBoard() {
 		const board = document.getElementById("board");
-		// Issues: 
-		// Gameboard was not being called, because Init was not being called in the first place
-		// Naming issues: CSS not taking effect because names were incorrect
-		
-		// const square = document.createElement("div");
-		// square.classList.add("square");
-		//square.classList.add("")
-		//board.appendChild(square); 
-		for (i = 0; i < 9; i++) {
+
+		for (i = 0; i < 12; i++) {
 			const square = document.createElement("div");
 			board.appendChild(square);
 			square.classList.add("square");
 		}
-		
 	},
 	
 	init() {
 		this.gameBoard();
 
 		document.getElementById("reset"); // Useful later
-		document.addEventListener("click", () => this.reset());
+		//document.addEventListener("click", () => this.reset());
 
 	}
-	
-	
-
 };
 
-ticTacToe.init();
+game.init();
 
 
 // So,  I'll need to create a game board
